2924d8a3ef17c806db1adbe6ed16208c079ac15d
MathSena <MathSena07@hotmail.com>
2023-01-03 22:28:13 -0300

    Creating header section

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,34 @@
+import HeaderImage from '../../assets/header.JPG'
 import React from 'react'
+import data from './data'
+import './header.css'
 
 const Header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+
+          <img src={HeaderImage} alt="Header Portait" />
+        </div>
+        <h3>Matheus Sena</h3>
+        <p>Hello! Welcome to my website! You are a click away from building your software today!
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'> Let's talk</a>
+          <a href="#portfolio" className='btn light'>My work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+
+          }
+        </div>
+
+      </div>
+      
+      
+      </header>
   )
 }
 
