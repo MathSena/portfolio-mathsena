17aad1167db52273df6850c48d842269fe4a199d
MathSena <MathSena07@hotmail.com>
2023-01-13 22:36:12 -0300

    Creating Floating Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,50 @@ import FAQs from './sections/faqs/FAQs';
 import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
 import Experiences from './sections/experience/Experiences';
+import FloatingNav from './sections/floating-nav/FloatingNav';
 import Modal from './components/Modal';
 import Theme from './theme/Theme';
 import { useThemeContext } from './context/theme-context';
+import {useRef, useState, useEffect} from 'react'
 
 const App = () => {
   const {themeState} = useThemeContext();
+
+  const mainRef = useRef();
+  const [showFloatingNav, setShowFloatingNav] = useState(true);
+  const [siteYPostion, setSiteYPosition] = useState(0)
+
+  const showFloatingNavHandler = () => {
+    setShowFloatingNav(true);
+  }
+
+  const hideFloatingNavHandler = () => {
+    setShowFloatingNav(false);
+  }
+
+  // check if floating nav should be shown or hidden
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const floatingNavToggleHandler = () => {
+    // check if we scrolled up or down at least 20px
+    if(siteYPostion < (mainRef?.current?.getBoundingClientRect().y - 20) || siteYPostion > (mainRef?.current?.getBoundingClientRect().y + 20)) {
+      showFloatingNavHandler();
+    } else {
+      hideFloatingNavHandler();
+    }
+
+    setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
+  }
+
+  useEffect(() => {
+    const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+
+    // cleanup function
+    return () => clearInterval(checkYPosition);
+  }, [floatingNavToggleHandler, siteYPostion])
+
+  
   return (
-    <main className={`${themeState.primary} ${themeState.background}`}>
+    <main className={`${themeState.primary} ${themeState.background}`} ref={mainRef}>
         <Navbar />
         <Header />
         <About />
@@ -26,7 +62,9 @@ const App = () => {
         <Footer />
         <Modal /> 
         <Theme />
-        {/* <FloatingNav /> */}
+        <FloatingNav />
+        {showFloatingNav && <FloatingNav/>}
+  
     </main>
   )
 }
