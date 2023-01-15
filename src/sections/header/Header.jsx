fa0057714e38629cb3daf2067b2e1142f18cf59f
MathSena <MathSena07@hotmail.com>
2023-01-14 23:56:16 -0300

    Putting animation

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,34 +1,36 @@
+import { useEffect } from 'react'
 import HeaderImage from '../../assets/header.JPG'
-import React from 'react'
 import data from './data'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import './header.css'
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({duration: 2000})
+  }, [])
+
   return (
     <header id="header">
       <div className="container header__container">
-        <div className="header__profile">
-
+        <div className="header__profile" data-aos="fade-in">
           <img src={HeaderImage} alt="Header Portait" />
         </div>
-        <h3>Matheus Sena</h3>
-        <p>Hello! Welcome to my website! You are a click away from building your software today!
+        <h3  data-aos="fade-up">Matheus Sena</h3>
+        <p  data-aos="fade-up">
+        Hello! Welcome to my website! You are a click away from building your software today!
         </p>
-        <div className="header__cta">
-          <a href="#contact" className='btn primary'> Let's talk</a>
-          <a href="#portfolio" className='btn light'>My work</a>
+        <div className="header__cta"  data-aos="fade-up">
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn light'>My Work</a>
         </div>
         <div className="header__socials">
           {
             data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-
           }
         </div>
-
       </div>
-      
-      
-      </header>
+    </header>
   )
 }
 
